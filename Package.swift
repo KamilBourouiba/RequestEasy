3e33990f649ed36222e683fc78c5d66f074c561e
Kamil <kbourouiba@icloud.com>
2024-06-30 02:12:15 +0200

    restructuration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
 // swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -6,11 +8,16 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15)
     ],
     products: [
         .library(
             name: "RequestEasy",
             targets: ["RequestEasy"]),
+    ],
+    dependencies: [
+        // No dependencies for now
     ],
     targets: [
         .target(
--- Sources/RequestEasy/RequestEasy.swift
+++ Sources/RequestEasy/RequestEasy.swift
@@ -1,84 +1,68 @@
 import Foundation
-
-#if canImport(UIKit)
 import UIKit
-#endif
 
 public enum ResponseType {
     case image
-    case images(count: Int)
-    case contentAsText(name: String)
-    case contentAsInt(name: String)
-    case contentAsJson
+    case text
 }
 
 public class RequestHandler {
-
+    
     public init() {}
-
-    public func GET(url: URL, type: ResponseType, completion: @escaping (Result<Any, Error>) -> Void) {
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
+    
+    public func GET() -> RequestGetter {
+        return RequestGetter()
+    }
+    
+    public class RequestGetter {
+        private var url: URL?
+        private var type: ResponseType = .text // Default type
+        
+        public func image(url: String) -> RequestGetter {
+            self.url = URL(string: url)
+            self.type = .image
+            return self
+        }
+        
+        public func text(url: String) -> RequestGetter {
+            self.url = URL(string: url)
+            self.type = .text
+            return self
+        }
+        
+        public func onCompletion(_ completion: @escaping (Result<Any, Error>) -> Void) {
+            guard let url = self.url else {
+                completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
                 return
             }
-
-            guard let data = data else {
-                completion(.failure(NSError(domain: "No data", code: -1, userInfo: nil)))
-                return
-            }
-
-            switch type {
-            case .image:
-                #if canImport(UIKit)
-                if let image = UIImage(data: data) {
-                    completion(.success(image))
-                } else {
-                    completion(.failure(NSError(domain: "Failed to create image", code: -1, userInfo: nil)))
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
                 }
-                #else
-                completion(.failure(NSError(domain: "UIImage not available on this platform", code: -1, userInfo: nil)))
-                #endif
-
-            case .images(let count):
-                #if canImport(UIKit)
-                var images: [UIImage] = []
-                for i in 0..<count {
+                
+                guard let data = data else {
+                    completion(.failure(NSError(domain: "No data", code: -1, userInfo: nil)))
+                    return
+                }
+                
+                switch self.type {
+                case .image:
                     if let image = UIImage(data: data) {
-                        images.append(image)
+                        completion(.success(image))
                     } else {
-                        completion(.failure(NSError(domain: "Failed to create image at index \(i)", code: -1, userInfo: nil)))
-                        return
+                        completion(.failure(NSError(domain: "Failed to create image", code: -1, userInfo: nil)))
+                    }
+                    
+                case .text:
+                    if let text = String(data: data, encoding: .utf8) {
+                        completion(.success(text))
+                    } else {
+                        completion(.failure(NSError(domain: "Failed to decode text", code: -1, userInfo: nil)))
                     }
                 }
-                completion(.success(images))
-                #else
-                completion(.failure(NSError(domain: "UIImage not available on this platform", code: -1, userInfo: nil)))
-                #endif
-
-            case .contentAsText(let name):
-                if let text = String(data: data, encoding: .utf8) {
-                    completion(.success([name: text]))
-                } else {
-                    completion(.failure(NSError(domain: "Failed to decode text", code: -1, userInfo: nil)))
-                }
-
-            case .contentAsInt(let name):
-                if let text = String(data: data, encoding: .utf8), let number = Int(text) {
-                    completion(.success([name: number]))
-                } else {
-                    completion(.failure(NSError(domain: "Failed to decode integer", code: -1, userInfo: nil)))
-                }
-
-            case .contentAsJson:
-                do {
-                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-                    completion(.success(jsonObject))
-                } catch {
-                    completion(.failure(error))
-                }
-            }
+            }.resume()
         }
-        task.resume()
     }
 }
