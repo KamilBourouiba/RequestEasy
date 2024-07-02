7a3f5aae360de8ca007fdf82a59fcbba80afa175
Kamil <kbourouiba@icloud.com>
2024-07-02 14:39:03 +0200

    hello LinkedIn

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RequestEasy",
     platforms: [
-        .iOS(.v14), // Adjust the version according to your requirements
+        .iOS(.v14),
     ],
     products: [
         .library(
@@ -14,8 +13,6 @@ let package = Package(
             targets: ["RequestEasy"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         .target(
